3b2457200e2c3921fcc05be9b4a4f1682c8e3b11
MrAFerreira <andre.f.v.ferreira@gmail.com>
2022-07-29 08:12:45 +0100

    adds ironbeers lab solution

---
+++ Week_4/Day_4/lab-ironbeers/app.js
@@ -0,0 +1,52 @@
+const express = require('express');
+
+const hbs = require('hbs');
+const path = require('path');
+const PunkAPIWrapper = require('punkapi-javascript-wrapper');
+
+const app = express();
+const punkAPI = new PunkAPIWrapper();
+
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Register the location for handlebars partials here:
+
+// ...
+
+// Add the route handlers here:
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+//app.get('/route', async ()=>{})
+
+app.get('/beers', async (req, res) => {
+  try {
+    let beersFromApi = await punkAPI.getBeers();
+
+    res.render('beers', { beers: beersFromApi });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.get('/random-beer', async (req, res) => {
+  try {
+    let beerFromApi = await punkAPI.getRandom();
+    let oneBeer = beerFromApi[0];
+
+    res.render('random-beer', oneBeer);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.listen(3000, () => console.log('🏃‍ on port 3000'));
+
+/* { 
+  beers:[{name: 'beer1'}, {name: 'beer2'}]
+}
+ */
